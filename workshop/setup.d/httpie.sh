0b9808391f3ef688421c4937e171de32c18387b4
Cora Iberkleid <ciberkleid@gmail.com>
2021-07-05 18:00:06 -0400

    try installing binaries from script

--- workshop/setup.d/httpie.sh
+++ workshop/setup.d/httpie.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# Install httpie
-virtualenv /home/eduk8s/bin/httpie
-source /home/eduk8s/bin/httpie/bin/activate
-pip install httpie
-deactivate
+## Install httpie
+#virtualenv /home/eduk8s/bin/httpie
+#source /home/eduk8s/bin/httpie/bin/activate
+#pip install httpie
+#deactivate
+#
+## Make sure the following line is in the file workshop/profile
+##alias http="/home/eduk8s/bin/httpie/bin/http"
 
-# Make sure the following line is in the file workshop/profile
-#alias http="/home/eduk8s/bin/httpie/bin/http"
-
-clear
+#clear
---
+++ workshop/setup.d/installer.sh
@@ -0,0 +1,32 @@
+echo "### Configuring git global settings"
+git config --global hub.protocol https
+git config --global credential.helper cache
+git config --global user.email "guest@example.com"
+git config --global user.name "Guest User"
+echo "### Finished configuring git global settings"
+
+echo "### Installing hub CLI"
+HUB_VERSION=2.14.2
+curl -L https://github.com/github/hub/releases/download/v${HUB_VERSION}/hub-linux-amd64-${HUB_VERSION}.tgz | tar zx && \
+     ./hub-linux-amd64-$HUB_VERSION/install && \
+     rm -rf hub-linux-amd64-$HUB_VERSION
+echo "### Finished installing hub CLI"
+
+echo "### Installing argocd CLI"
+#ARGOCD_VERSION=2.0.4
+#https://github.com/argoproj/argo-cd/releases/download/v2.0.4/argocd-linux-amd64
+VERSION=$(curl --silent "https://api.github.com/repos/argoproj/argo-cd/releases/latest" | grep '"tag_name"' | sed -E 's/.*"([^"]+)".*/\1/')
+curl -sSL -o /home/eduk8s/bin/argocd https://github.com/argoproj/argo-cd/releases/download/$VERSION/argocd-linux-amd64
+chmod +x /home/eduk8s/bin/argocd
+echo "### Finished installing argocd CLI"
+
+echo "### Installing httpie CLI"
+virtualenv /home/eduk8s/bin/httpie
+source /home/eduk8s/bin/httpie/bin/activate
+pip install httpie
+deactivate
+# Make sure the following line is in the file workshop/profile
+#alias http="/home/eduk8s/bin/httpie/bin/http"
+echo "### Finished installing httpie CLI"
+
+clear
