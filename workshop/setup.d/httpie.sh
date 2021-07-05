6a436b8c677e9258c1fb5558179ae58d4d6398ee
Cora Iberkleid <ciberkleid@gmail.com>
2021-07-05 12:34:58 -0400

    fix setting http alias

--- workshop/setup.d/httpie.sh
+++ workshop/setup.d/httpie.sh
@@ -6,6 +6,6 @@ pip install httpie
 deactivate
 
 # Make sure the following line is in the file workshop/profile
-#echo 'alias http="/home/eduk8s/bin/httpie/bin/http"' >> ~/.bash_profile
+#alias http="/home/eduk8s/bin/httpie/bin/http"
 
 clear
