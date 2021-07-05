434065e727cd6ad8d2b29d74036bd4f47bce83f0
Cora Iberkleid <ciberkleid@gmail.com>
2021-07-05 10:04:08 -0400

    fix scripts after testing

--- fork-repos.sh
+++ fork-repos.sh
@@ -1,60 +1,58 @@
 #!/usr/bin/bash
 
-export REPO=cat-service
-git ls-remote https://github.com/${GITHUB_ORG}/${REPO} &>/dev/null
-exit_code=$?
-if [[ $exit_code == 0 ]]; then
-    echo -e "Repository https://github.com/booternetes-III-springonetour-july-2021/${REPO} exists."
-    echo -e "Please delete it, and then re-run this script."
-    echo -e "You can delete the repo from the GitHub UI, or by typing:"
-    echo -e "  hub delete ${GITHUB_ORG}/${REPO} -y"
-else
-    hub clone https://github.com/booternetes-III-springonetour-july-2021/${REPO} && cd ${REPO}
-    hub fork --remote-name origin
-    sed -i '' "s/booternetes-III-springonetour-july-2021/${GITHUB_ORG}/g" .github/workflows/deploy.sh
-    sed -i '' "s/mvn clean deploy/mvn clean package/g" .github/workflows/deploy.sh
-    git add .github/workflows/deploy.sh
-    git commit -m "Update GitHub org. Use mvn package instead of deploy."
-    git push --set-upstream origin main
-    cd ..
+# Check to see if repos exist
+
+repos=('cat-service' 'cat-service-release' 'cat-service-release-ops')
+error_flag=0
+
+for repo in "${repos[@]}"
+do
+  git ls-remote https://github.com/${GITHUB_ORG}/${repo} &>/dev/null
+  error_code=$?
+  if [[ $error_code == 0 ]]; then
+    error_flag=1
+    echo -e "ERROR: Repository https://github.com/${GITHUB_ORG}/${repo} already exists."
+  fi
+done
+
+if [[ $error_flag == 1 ]]; then
+  echo
+  echo "!!!!! ERROR !!!!!"
+  echo
+  echo "Please delete the existing repositories and re-run this script."
+  echo "You can delete the repos from the GitHub UI, or by typing:"
+  echo "  hub delete ${GITHUB_ORG}/<repo-name>"
+  return
 fi
 
-export REPO=cat-service-release
-# Clone and fork repo. Set branch as main.
-git ls-remote https://github.com/${GITHUB_ORG}/${REPO} &>/dev/null
-exit_code=$?
-if [[ $exit_code == 0 ]]; then
-    echo -e "Repository https://github.com/booternetes-III-springonetour-july-2021/${REPO} exists."
-    echo -e "Please delete it, and then re-run this script."
-    echo -e "You can delete the repo from the GitHub UI, or by typing:"
-    echo -e "  hub delete ${GITHUB_ORG}/${REPO} -y"
-else
-    hub clone https://github.com/booternetes-III-springonetour-july-2021/${REPO} \
-      && cd ${REPO} \
-      && hub fork --remote-name origin \
-      && cd .. \
-      && rm -rf ${REPO}
-fi
+# Fork and modify repos
 
-export REPO=cat-service-release-ops
-# Clone and fork repo. Set branch as main.
-git ls-remote https://github.com/${GITHUB_ORG}/${REPO} &>/dev/null
-exit_code=$?
-if [[ $exit_code == 0 ]]; then
-    echo -e "Repository https://github.com/booternetes-III-springonetour-july-2021/${REPO} exists."
-    echo -e "Please delete it, and then re-run this script."
-    echo -e "You can delete the repo from the GitHub UI, or by typing:"
-    echo -e "  hub delete ${GITHUB_ORG}/${REPO} -y"
-else
-    hub clone https://github.com/booternetes-III-springonetour-july-2021/${REPO} && cd ${REPO}
-    hub fork --remote-name origin
-    rm *.sh
-    rm manifests/overlays/dev/.argocd-source-dev-cat-service.yaml
-	rm manifests/overlays/prod/.argocd-source-prod-cat-service.yaml
-    find . -name *.yaml -exec sed -i "s/booternetes-III-springonetour-july-2021/${GITHUB_ORG}/g" {} +
-    find . -name *.yaml -exec sed -i "s/gcr\.io\/pgtm-jlong/${REGISTRY_HOST}/g" {} +
-    git add -A
-    git commit -m "Update GitHub org and Docker registry. Remove unnecessary files."
-    git push --set-upstream origin main
-    cd ..
-fi
+# cat-service
+hub clone https://github.com/booternetes-III-springonetour-july-2021/cat-service && cd cat-service
+hub fork --remote-name origin
+sed -i '' "s/booternetes-III-springonetour-july-2021/${GITHUB_ORG}/g" .github/workflows/deploy.sh
+sed -i '' "s/mvn clean deploy/mvn clean package/g" .github/workflows/deploy.sh
+git add .github/workflows/deploy.sh
+git commit -m "Update GitHub org. Use mvn package instead of deploy."
+git push --set-upstream origin main
+cd ..
+
+#cat-service-release
+hub clone https://github.com/booternetes-III-springonetour-july-2021/cat-service-release \
+  && cd cat-service-release \
+  && hub fork --remote-name origin \
+  && cd .. \
+  && rm -rf cat-service-release
+
+# cat-service-release-ops
+hub clone https://github.com/booternetes-III-springonetour-july-2021/cat-service-release-ops && cd cat-service-release-ops
+hub fork --remote-name origin
+rm *.sh
+rm manifests/overlays/dev/.argocd-source-dev-cat-service.yaml
+rm manifests/overlays/prod/.argocd-source-prod-cat-service.yaml
+find . -name *.yaml -exec sed -i "s/booternetes-III-springonetour-july-2021/${GITHUB_ORG}/g" {} +
+find . -name *.yaml -exec sed -i "s/gcr\.io\/pgtm-jlong/${REGISTRY_HOST}/g" {} +
+git add -A
+git commit -m "Update GitHub org and Docker registry. Remove unnecessary files."
+git push --set-upstream origin main
+cd ..
--- set-credentials.sh
+++ set-credentials.sh
@@ -6,7 +6,7 @@
 # Do not change the names of the GITHUB env vars
 # hub CLI uses GITHUB_USERNAME and GITHUB_TOKEN env vars
 
-echo -n "Enter your GitHub username [${GITHUB_USERNAME}]: " && read GITHUB_USERNAME
+echo -n "Enter your GitHub username: " && read GITHUB_USERNAME
 export GITHUB_USERNAME
 
 echo -n "Enter your GitHub auth token: " && read -s GITHUB_TOKEN || { stty -echo; read GITHUB_TOKEN; stty echo; }
@@ -17,4 +17,13 @@ echo -n "Enter your GitHub org name [${GITHUB_USERNAME}]: " && read GITHUB_ORG
 GITHUB_ORG="${GITHUB_ORG:-$GITHUB_USERNAME}"
 export GITHUB_ORG
 echo
+
+if [[ ! "${GITHUB_USERNAME}" == ""  && ! "${GITHUB_TOKEN}" == ""  && ! "${GITHUB_ORG}" == "" ]]; then
+  echo "Thank you. GITHUB_USERNAME, GITHUB_TOKEN, and GITHUB_ORG have been set in this terminal."
+else
+  echo "!!!!! ERROR !!!!!"
+  echo
+  echo "GITHUB_USERNAME, GITHUB_TOKEN, and/or GITHUB_ORG have not been properly set in this terminal."
+  echo "Please re-run this script and enter all values."
+fi
 
