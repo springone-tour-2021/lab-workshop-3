fd87d40f27b36d48138cbaa844000e2255dea10c
Cora Iberkleid <ciberkleid@gmail.com>
2021-07-05 21:44:57 -0400

    set upstream for all clones

--- scripts/fork-repos.sh
+++ scripts/fork-repos.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-# Check to see if repos exist
+echo -e "\nCheck to see if repos exist"
 
 repos=('cat-service' 'cat-service-release' 'cat-service-release-ops')
 error_flag=0
@@ -11,25 +11,22 @@ do
   error_code=$?
   if [[ $error_code == 0 ]]; then
     error_flag=1
-    echo -e "ERROR: Repository https://github.com/${GITHUB_ORG}/${repo} already exists."
+    echo "ERROR: Repository https://github.com/${GITHUB_ORG}/${repo} already exists."
   fi
 done
 
 if [[ $error_flag == 1 ]]; then
-  echo
-  echo "!!!!! ERROR !!!!!"
-  echo
+  echo -e "\n!!!!! ERROR !!!!!\n"
   echo "Please delete the existing repositories and re-run this script."
   echo "You can delete the repos from https://github.com/${GITHUB_ORG}, or by typing:"
   echo "  hub delete ${GITHUB_ORG}/<repo-name> (requires delete_repo rights)"
   return
 fi
 
-# Fork and modify repos
-
-# cat-service
+echo -e "\nClone, fork and modify repo: cat-service"
 hub clone https://github.com/booternetes-III-springonetour-july-2021/cat-service && cd cat-service
 hub fork --remote-name origin
+git branch --set-upstream-to origin/main
 sed -i "s/booternetes-III-springonetour-july-2021/${GITHUB_ORG}/g" .github/workflows/deploy.sh
 sed -i "s/mvn clean deploy/mvn clean package/g" .github/workflows/deploy.sh
 git add .github/workflows/deploy.sh
@@ -37,16 +34,17 @@ git commit -m "Update GitHub org. Use mvn package instead of deploy."
 git push --set-upstream origin main
 cd ..
 
-#cat-service-release
-hub clone https://github.com/booternetes-III-springonetour-july-2021/cat-service-release \
-  && cd cat-service-release \
-  && hub fork --remote-name origin \
-  && cd .. \
-  && rm -rf cat-service-release
+echo -e "\nClone, fork and modify repo: cat-service-release"
+hub clone https://github.com/booternetes-III-springonetour-july-2021/cat-service-release && cd cat-service-release
+hub fork --remote-name origin
+git branch --set-upstream-to origin/release
+cd ..
+rm -rf cat-service-release
 
-# cat-service-release-ops
+echo -e "\nClone, fork and modify repo: cat-service-release-ops"
 hub clone https://github.com/booternetes-III-springonetour-july-2021/cat-service-release-ops && cd cat-service-release-ops
 hub fork --remote-name origin
+git branch --set-upstream-to origin/main
 rm *.sh
 rm manifests/overlays/dev/.argocd-source-dev-cat-service.yaml
 rm manifests/overlays/prod/.argocd-source-prod-cat-service.yaml
