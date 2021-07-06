a08f97fdde5c72a126bebb9786f5d540d2153da8
Cora Iberkleid <ciberkleid@gmail.com>
2021-07-05 21:07:35 -0400

    log output

--- workshop/setup.d/installer.sh
+++ workshop/setup.d/installer.sh
@@ -1,18 +1,22 @@
-echo "### Configuring git global settings"
+#!/bin/bash
+
+{
+
+echo -e "\n### Configuring git global settings"
 git config --global hub.protocol https
 git config --global credential.helper cache
 git config --global user.email "guest@example.com"
 git config --global user.name "Guest User"
 echo "### Finished configuring git global settings"
 
-echo "### Installing hub CLI"
+echo -e "\n### Installing hub CLI"
 HUB_VERSION=2.14.2
 curl -L https://github.com/github/hub/releases/download/v${HUB_VERSION}/hub-linux-amd64-${HUB_VERSION}.tgz | tar zx && \
      mv ./hub-linux-amd64-${HUB_VERSION}/bin/hub /home/eduk8s/bin && \
      rm -rf hub-linux-amd64-$HUB_VERSION
 echo "### Finished installing hub CLI"
 
-echo "### Installing argocd CLI"
+echo -e "\n### Installing argocd CLI"
 #ARGOCD_VERSION=2.0.4
 #https://github.com/argoproj/argo-cd/releases/download/v${ARGOCD_VERSION}/argocd-linux-amd64
 VERSION=$(curl --silent "https://api.github.com/repos/argoproj/argo-cd/releases/latest" | grep '"tag_name"' | sed -E 's/.*"([^"]+)".*/\1/')
@@ -20,7 +24,7 @@ curl -sSL -o /home/eduk8s/bin/argocd https://github.com/argoproj/argo-cd/release
 chmod +x /home/eduk8s/bin/argocd
 echo "### Finished installing argocd CLI"
 
-echo "### Installing httpie CLI"
+echo -e "\n### Installing httpie CLI"
 virtualenv /home/eduk8s/bin/httpie
 source /home/eduk8s/bin/httpie/bin/activate
 pip install httpie
@@ -30,3 +34,5 @@ deactivate
 echo "### Finished installing httpie CLI"
 
 clear
+
+} > 2>&1 | tee installer.log
